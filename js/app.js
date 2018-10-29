97fc73e5a68eae9576cf7afadbacdc83075610ee
Terri Bedore <terribedore@gmail.com>
2018-10-29 14:28:48 -0500

    reminder of what 'cb' stands for.

--- js/app.js
+++ js/app.js
@@ -40,7 +40,7 @@ function init() {
  * This function all supports a callback as the second parameter
  * which will be called after everything has run successfully.
  */
- function loadFeed(id, cb) {
+ function loadFeed(id, cb) { // NOTE: cb = callbacks
      var feedUrl = allFeeds[id].url,
          feedName = allFeeds[id].name;
 
